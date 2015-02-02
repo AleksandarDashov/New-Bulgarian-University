ddca5fa37973a44e8b2f81ae7d17c16e91a3554b
Aleksandar Dashov <GrimReaperBG20@gmail.com>
2015-02-02 15:07:37 +0200

    Rando.h
    The header file for the random cipher.

---
+++ NETB141-Project/Random-Cipher/Rando.h
@@ -0,0 +1,25 @@
+#ifndef RANDO_H
+#define RANDO_H
+#include <string>
+using namespace std;
+
+class Rando
+{
+	public:
+		Rando();
+		char randLetter();
+		string cipherGenerator(); //substituting the alphabet with random letters
+		string setSentence(); //set the sentence
+		string getSentence(); //return the sentence
+		string encode(); //encoding english message into ciphered message
+		string decode(); //decoding ciphered message into english message
+		int getLettersSize();
+	private:
+		string letters;
+		string alphabet;
+		string sentence;
+		string cipher;
+	
+};
+
+#endif
