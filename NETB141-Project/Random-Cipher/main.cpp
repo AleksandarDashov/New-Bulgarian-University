5f984afeb28f9e245a1bffb2259ced9de09ffc7d
Aleksandar Dashov <GrimReaperBG20@gmail.com>
2015-02-02 15:47:31 +0200

    Done

--- NETB141-Project/Random-Cipher/main.cpp
+++ NETB141-Project/Random-Cipher/main.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
-#include <ctime>
 #include <cstdlib>
 #include "Rando.h"
 
 using namespace std;
 
-
-
 int main()
 {
 	srand(time(NULL));
 	Rando klon;
-	klon.cipherGenerator();
-	klon.setSentence();
+	klon.cipherGenerator();//The program generates a cipher by replacing each Latin letter
+	klon.setSentence(); //The program reads a message in English from the standard input
 	klon.getSentence();
-	klon.encode();
-	klon.decode();
-
-
+	klon.encode(); //encodes it using the random cipher
+	klon.setSentence();//The program reads a ciphered message from the standard input
+	klon.getSentence();
+	klon.decode(); //decodes it using the same cipher
+	
 	return 0;
 }
